a254261fd76a0bd7cbc7d4e25e5e338b4259088d
hejiawang <952327407@qq.com>
2015-10-19 15:55:03 +0800

    文件的输入输出，数组——指针的进一步理解

--- FirstCPP/HelloCPP.cpp
+++ FirstCPP/HelloCPP.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 /*
 	函数原型(函数声明),函数原型描述了函数的接口。
 	C++规定可以先说明函数原型，然后就可以调用函数。函数定义可放在程序后面。
 	在本例中，如果Simon函数声明在main之前，就不用函数声明了。
-*/
+	*/
 void simon(int);
 void stringAndArray();
 void stringType();
 void myInflatable();
 void pointer();
+void cin1();
+void fileTest();
+int fill_array(double ar[], int limit);
+void show_array(const double ar[], int n);
 
 //结构
 struct inflatable{
@@ -20,19 +25,26 @@ struct inflatable{
 };
 
 int main(){
-	
+
 	cout << "my first C++ project";
 	cout << endl;	//另起一行
 	cout << "hello C++" << endl;
-	
+
 	//int b;
 	//cin >> b;	//将控制台输入的值赋给b
 	//simon(b);	//自定义函数
-	
+
 	//stringAndArray();
 	//stringType();
 	//myInflatable();//结构
-	pointer();//指针
+	//pointer();//指针
+	//cin1();//使用原始的cin进行输入
+	//fileTest();//从控制台输入的信息记录到文件中
+	
+	const int max = 5;
+	double propertise[max];
+	int size = fill_array( propertise, max );
+	show_array(propertise, max);
 
 	cin.get();
 	cin.get();
@@ -40,12 +52,12 @@ int main(){
 }
 
 void simon(int b){
-	
-	cout << "a int num is " << b <<endl;
+
+	cout << "a int num is " << b << endl;
 }
 
 void stringAndArray(){
-	
+
 	const int Size = 15;
 	char name1[Size] = "c++ array";
 	cout << name1 << endl;
@@ -96,10 +108,104 @@ void pointer(){
 
 	cout << "Values : updates = " << updates;
 	cout << " , *p_updates = " << *p_updates << endl;
-	
+
 	cout << "Addresses : updates = " << &updates;
 	cout << " , p_updates = " << p_updates << endl;
 
 	*p_updates = *p_updates + 1;
 	cout << "Now updates = " << updates << endl;
+}
+
+void cin1(){
+
+	char ch;
+	int count = 0;
+	cout << "Enter characters; enter # to-quit:\n";
+	cin >> ch;	//不能识别出空格、换行
+	cin.get(ch);	//能够识别出空格、换行
+	//while ( ch != '#' ) {
+	/*while (cin.fail() == false){	//检测文件尾，在win7中，要按Ctrl+z，然后Enter。
+		cout << ch;
+		++count;
+		//cin >> ch;	//读取下一个字符
+		cin.get(ch);	//读取下一个字符
+		}*/
+	while (cin.get(ch)){
+		cout << ch;
+		++count;
+	}
+	cout << endl << count << " characters read \n";
+}
+
+void fileTest(){
+
+	char automobile[50];
+	int year;
+	double a_price;
+	double d_price;
+
+	ofstream outFile;
+	outFile.open("E:\\test.txt");
+
+	cout << " Enter the make and model of automonile: ";
+	cin.getline(automobile, 50);
+	cout << " Enter the model year: ";
+	cin >> year;
+	cout << " Entry the original asking price: ";
+	cin >> a_price;
+	d_price = 0.913 * a_price;
+
+	cout << fixed;
+	cout.precision(2);
+	cout.setf(ios_base::showpoint);
+	cout << " Make and model:" << automobile << endl;
+	cout << " Year: " << year << endl;
+	cout << " Was asking $ " << a_price;
+	cout << " Now asking $ " << d_price;
+
+	//输入到文件中
+	outFile << fixed;
+	outFile.precision(2);
+	outFile.setf(ios_base::showpoint);
+	outFile << " Make and model:" << automobile << endl;
+	outFile << " Year: " << year << endl;
+	outFile << " Was asking $ " << a_price;
+	outFile << " Now asking $ " << d_price;
+	outFile.close();
+}
+
+int fill_array( double ar[], int limit){
+
+	//int fill_array( const double ar[], int limit ){//const修饰常亮数据，意味着，不能修改ar
+	double temp;
+	int i;
+	for (i = 0; i < limit; i++) {
+		
+		cout << " Enter value # " << (i+1) << ": ";
+		cin >> temp;
+		if ( !cin ){	//输入的不合法
+			
+			cin.clear();//清除输入的内容
+			while ( cin.get() != '\n' ){
+				
+				continue;
+			}
+			cout << " Bad input; input process terminated. \n ";
+			break;
+		} else if ( temp < 0 ){
+			
+			break;
+		}
+		ar[i] = temp;
+	}
+	return i;
+}
+
+void show_array( const double ar[], int n ){ //显示数组内容
+	
+	for (int i = 0; i < n; i++){
+		
+		cout << " Property # "<< (i+1) << " :$ ";
+		cout << ar[i] << endl;	
+	}
 }
